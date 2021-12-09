d3a4fcfd93852814ab731c11550e1507b41c6699
geza-s <geza.soldati@epfl.ch>
2021-12-09 13:16:31 +0100

    image

--- code_assignment/part1/matlab/analyze_image_with_FFT.m
+++ code_assignment/part1/matlab/analyze_image_with_FFT.m
@@ -18,8 +18,8 @@ imshow(pic)
 
 
 %% Prepare for FFT
-pixelRows = pic(4,:); % modify this according to your strategy
-pixelCols = pic(:,4); % modify this according to your strategy
+pixelRows = pic(20,:); % modify this according to your strategy
+pixelCols = pic(:,40); % modify this according to your strategy
 
 
 %% Take fft of rows and columns
@@ -33,12 +33,17 @@ Fmag_cols = real(Fcols).^2+imag(Fcols).^2;
 Fmag_cols_plot = fftshift(Fmag_cols);
 x_cols = (0:length(Fmag_cols)-1) - floor(length(Fmag_cols)/2);
 
+figure
 plot(x_rows, Fmag_rows_plot)
 hold on
 plot(x_cols, Fmag_cols_plot)
 legend('rows', 'cols')
 grid on
 
+figure
+histogram(Fmag_rows_plot);
+hold on;
+histogram(Fmag_cols_plot);
 
 %% Analyze image - Question 2.2
 black=0; % should be toggled to 1 if black is detected
@@ -47,17 +52,42 @@ horizontal=0; % should be toggled to 1 if horizontal is detected
 
 %****************** your image analysis *********************%
 
-threshold = median(Fmag_rows_plot)*100;
+threshold = 1000000;
 
-h = (Fmag_rows_plot>threshold);
+h1 = (Fmag_rows_plot>threshold);
+h2 = (Fmag_cols_plot>threshold)';
 
+figure
+bar(h1)
+hold on
+bar(h2)
 
-m = 
+hf1 = zeros([1,length(h1)]); %array of i+1 shifted
+hf1(2:length(h1)) = h1(1:length(h1)-1);
+dh1 = length(h1(abs(hf1-h1)>0))/2;
+disp("peaks in rows fft: "+dh1)
+
+hf2 = zeros([1,length(h2)]);
+hf2(2:length(h2)) = h2(1:length(h2)-1);
+dh2 = length(h2(abs(hf2-h2)>0))/2;
+disp("peaks in cols fft: " +dh2)
 
 
 %************************************************************%
 
+delta = dh2 - dh1;
 
+switch delta
+    case 0
+        disp('black')
+        black = 1;
+    case 2
+        disp("Horizontal stripped")
+        horizontal = 1;
+    case 3
+        disp("Vertical stripped")
+        vertical = 1;
+end;
 
 
 %% Plots
